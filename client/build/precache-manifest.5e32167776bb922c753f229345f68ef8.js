e9de42b94784c77aa8de9e9faa93100816a06056
unknown <bvi1@ucsc.edu>
2019-12-19 01:43:05 -0800

    adding build

---
+++ client/build/precache-manifest.5e32167776bb922c753f229345f68ef8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c173d93272ad0532d2587d3e27d15762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e474f1a10a79b4139c",
+    "url": "/static/css/2.8b337d20.chunk.css"
+  },
+  {
+    "revision": "31e4f0cead0734f12b3a",
+    "url": "/static/css/main.948cb523.chunk.css"
+  },
+  {
+    "revision": "a5e474f1a10a79b4139c",
+    "url": "/static/js/2.379eb87d.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/static/js/2.379eb87d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31e4f0cead0734f12b3a",
+    "url": "/static/js/main.0fcaa9eb.chunk.js"
+  },
+  {
+    "revision": "f0f2dcd0fc064f559b7c",
+    "url": "/static/js/runtime-main.ca901d7f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
