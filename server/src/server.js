55482ba1f9a7852f072f0775da2576b77a187599
unknown <bvi1@ucsc.edu>
2019-12-19 01:23:57 -0800

    blah

--- server/src/server.js
+++ server/src/server.js
@@ -144,7 +144,7 @@ app.listen(process.env.PORT || 3002, () => {
 });
 
 if(process.env.NODE_ENV === 'production'){
-  // app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
