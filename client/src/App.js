de49672b372401cffcfd4851b0f7e370cb2f90ee
unknown <bvi1@ucsc.edu>
2019-12-18 16:14:50 -0800

    added some frontend changes like the faq

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import './App.css'
 import Main from './components/Main'
+import FAQComponent from './components/FAQComponent/ComponentFAQ'
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
+      <div>
+        <div className="App">
+          <Main />
+        </div>
+        <center className="Disclaimer">
+          This application is NOT run by Caltrain or any of its affliates or contractors. Created by <a href={`https://www.linkedin.com/in/bvi1994/}`}>Brandon Vi</a> Help me improve the application by getting the <a href={`https://github.com/bvi1994/caltrain-widget`}>source code</a>
+          <FAQComponent />
+        </center>
       </div>
     );
   }
---
+++ client/src/components/FAQComponent/ComponentFAQ.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import parse from 'html-react-parser';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+import 'react-accessible-accordion/dist/fancy-example.css';
+import './styles.css'
+
+class FAQComponent extends Component {
+  render(){
+    const FAQ = [
+      {
+        question: `Who create this application?`,
+        answer: `This application is created by ${`<a href='https://www.linkedin.com/in/bvi1994/'> Brandon Vi</a>`}. Check him out and he is available for hire. ;)`,
+      },
+      {
+        question: `Do you have the source code for the this application? I have ways to make it better!`,
+        answer: `Great! The code can be found <a href='https://github.com/bvi1994/caltrain-widget'>here!</a> I will have a set up guide in the next version of the application.`,
+      },
+      {
+        question: `What are some of the features you will be working on for the next version?`,
+        answer: `Real time integration with the 511.org, automatically going to the closest trip time based on current time, better code doocumentation on the README for developers, notifcation of caltrain problems, just to name a few...`
+      },
+      {
+        question: `There's a bug on your application! Where do I submit bug reports?`,
+        answer: `You can send me an email to brandvi1994 [at] gmail (dot) com, describe the bug and I will get to it.`
+      },
+      {
+        question: `What else should I know?`,
+        answer: `This application is not developed by Caltrain. This is a free service being managed by a geek in his dwindling spare time. It’s provided “as-is” and there are no guarantees (expressed or implied).`
+      },
+    ];
+    return(
+      <div className={`dropdownFAQ`}>
+        <Accordion>
+        {FAQ.map(question =>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                {parse(question.question)}
+                </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                <p align='left'>
+                  {parse(question.answer)}
+                </p>
+              </AccordionItemPanel>
+            </AccordionItem>
+        )}
+        </Accordion>
+      </div>
+    )
+  }
+}
+
+export default FAQComponent;
